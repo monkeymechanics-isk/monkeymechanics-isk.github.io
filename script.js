d901c098a78fcabaa7b37ff0baa34362c3be3cb6
Monkey Mechanics ISK <mehlam.turra2014@gmail.com>
2025-11-05 15:49:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// MOBILE MENU
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector("nav ul");
-
-menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
-
-// POPUP FORM
-const popup = document.getElementById("popupForm");
-const openBtn = document.getElementById("openFormBtn");
-const closeBtn = document.getElementById("closeBtn");
-
-if (openBtn) {
-  openBtn.addEventListener("click", () => popup.style.display = "block");
+function openForm() {
+  document.getElementById("formPopup").style.display = "block";
 }
 
-if (closeBtn) {
-  closeBtn.addEventListener("click", () => popup.style.display = "none");
+function closeForm() {
+  document.getElementById("formPopup").style.display = "none";
 }
