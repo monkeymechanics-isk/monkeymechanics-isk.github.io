4922a385c8a4c4096e33259fc7caff2cb6de155c
Monkey Mechanics ISK <mehlam.turra2014@gmail.com>
2025-11-22 21:50:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
-@@ -1,7 +1 @@
+// Open the popup form
 function openForm() {
   document.getElementById("formPopup").style.display = "block";
 }
 
+// Close the popup form
 function closeForm() {
   document.getElementById("formPopup").style.display = "none";
 }
